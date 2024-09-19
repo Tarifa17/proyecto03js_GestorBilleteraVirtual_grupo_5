3d6af5a7750eab0e808b7cfb5e1ebb03283a1cbe
MateoTarifa <mtarifa17@gmail.com>
2024-09-19 17:11:08 -0300

    Update.BilleterasVirtuales
    Adición  del routing

--- server.js
+++ server.js
@@ -13,7 +13,9 @@ const app = express();
     });
 
 //routing
-
+app.use('/src', express.static(__dirname+"/src"));
+app.use('/public',express.static(__dirname+"/public"));
+app.use('/node_modules',express.static(__dirname+"node_modules"))
 //listening 
     app.listen(5000, () =>{
         console.log("Servidor corriendo");
