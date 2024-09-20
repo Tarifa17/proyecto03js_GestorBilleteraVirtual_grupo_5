9a6abfb30d540929a71ca5738bfee79369b441d2
Rubenxdf <165188410+Rubenxdf@users.noreply.github.com>
2024-09-19 22:07:07 -0300

    cambios css
    modificando la carpeta css y implementamiento de una funcion que borre los datos ingresados ..

--- src/index.js
+++ src/index.js
@@ -26,26 +26,22 @@ function guardarDatos(){
     listaUsuarios.push(usuario);
     listaBilleteras.push(billetera);
     listaTransacciones.push(transaccion);
+    document.getElementById('usuario').value = ""; //borra los valores ingresados en el usuario
+    document.getElementById('billetera').value = ""; //borra los valores ingresados en  billetera
+    document.getElementById('transaccion').value = ""; //borra los valores ingresados en  transaccion
     mostarLista();
 }
 
-function mostrarDatos(){
-    //const lista = [...listaUsuarios,...listaBilleteras,...listaTransacciones];
-    //let modeloTabla = '<li></li>';
-    var lista=document.getElementById("lista"); 
-    //modeloTabla = modeloTabla + '<tr> <th>Nombre Usuario</th> <th>Billetera</th> <th>N° Transacciones</th> </tr>';
-    for (let i = 0; i<=listaUsuarios.length; i++) {
-        //modeloTabla = modeloTabla + '<tr>';
-       // modeloTabla = modeloTabla + '<td>' + listaUsuarios[i] + '</td';
-        //modeloTabla = modeloTabla + '<td>' + listaBilleteras[i] + '</td';
-        //modeloTabla = modeloTabla + '</tr';
-        let linew= document.createElement("table");    
-        let contenido = document.createTextNode(listaUsuarios[i]+' '+listaBilleteras[i]+" "+listaTransacciones[i]);
-        lista.appendChild(linew);
-        linew.appendChild(contenido);
-    };
 
-    //modeloTabla = modeloTabla + '</table';
-    //document.getElementById('lista').innerHTML=modeloTabla;
+function mostrarDatos(){
+    var lista = document.getElementById("lista");
+    lista.innerHTML = ""; // Limpiar el contenido anterior
+
+    for (let i = 0; i < listaUsuarios.length; i++) {
+        let item = document.createElement("li");
+        item.innerText = listaUsuarios[i] + " - " + listaBilleteras[i] + " - " + listaTransacciones[i];
+        lista.appendChild(item);
+        
+    }
 }
 
